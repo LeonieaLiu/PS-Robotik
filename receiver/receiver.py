1eec38b2f06463e09fee569b7f96f4c36448f408
Jinghui Liu <jinghui.liu@stud.tu-darmstadt.de>
2024-06-17 15:51:12 +0200

    receiver_demo1

---
+++ receiver/receiver.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float32, Float64MultiArray
+
+
+def angle_callback(msg):
+    rospy.loginfo("Received angle: {}".format(msg.data))
+
+
+def matrix_callback(msg):
+    rospy.loginfo("Received matrix: {}".format(msg.data))
+
+if __name__ == '__main__':
+    rospy.init_node('receiver')
+    angle_sub = rospy.Subscriber('/jetbot_angle', Float32, angle_callback, queue_size=1)
+    matrix_sub = rospy.Subscriber('/adjusted_matrix', Float64MultiArray, matrix_callback, queue_size=1)
+    rospy.spin()
